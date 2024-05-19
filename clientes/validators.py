c141fecec24064395a192e7fab7ce1be82fee4ba
Jefferson Lopes <jeffbreno@gmail.com>
2024-05-19 16:02:38 -0300

    projeto django framework rest 18052024_1602

---
+++ clientes/validators.py
@@ -0,0 +1,17 @@
+import re
+from validate_docbr import CPF
+
+def cpf_valido(numero_cpf):
+    cpf = CPF()
+    return cpf.validate(numero_cpf)
+    
+def nome_valido(nome):
+    return re.match("^[A-Za-z ]+$", nome)
+
+def rg_valido(rg):
+    return len(rg) == 9
+
+def celular_valido(celular):
+    modelo = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    resposta = re.findall(modelo, celular)
+    return resposta
